847af5e64981ee07bb31991b7e338b807f9f382d
Jaco Geldenhuys <jgeldenhuys@bittitan.com>
2021-07-22 09:09:03 -0700

    commit

--- my-package/tasks/save-data/save-data.ps1
+++ my-package/tasks/save-data/save-data.ps1
@@ -4,3 +4,11 @@ param ([string]$customUri, [string]$myData)
 $VerbosePreference='Continue'
 
 Write-Information "Getting random data: '$customUri', '$myData'"
+
+$json = "{ 'id': '1', 'static': 'STATIC', 'random': '$myData' }"
+
+Write-Information "Writing data..."
+
+Write-VoleerData -TypeName "MyData" -Items $json
+
+Write-Information "DONE"
