1762ea386de8da7a9477b89698008bc167f94d91
Jaco Geldenhuys <jgeldenhuys@bittitan.com>
2021-11-01 16:18:41 -0700

    =checkpoint

--- my-package/tasks/exec/exec.ps1
+++ my-package/tasks/exec/exec.ps1
@@ -1,9 +1,19 @@
 
-Write-Information "Exec Started"
+Write-Information "Started"
 
-try {
-  Invoke-RestMethod -Uri "https://api.sendgrid.com/v3/mail/send"
-}
-catch {
-  Write-Warning "Failed: $_"
-}
+Write-Warning "THIS IS A WARNING!"
+
+Start-Sleep -Seconds 0.25
+
+Write-Error "THIS IS A ERROR!"
+
+# Write-Information "Exec Started"
+
+# try {
+#   Invoke-RestMethod -Uri "https://api.sendgrid.com/v3/mail/send"
+# }
+# catch {
+#   Write-Warning "Failed: $_"
+# }
+
+# Set-VoleerRunSummary -Summary "THIS IS THE RUN SUMMARY!"
