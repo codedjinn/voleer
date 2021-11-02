4a3cd573c71ede938665866557b27e3113a2cd1c
Jaco Geldenhuys <jgeldenhuys@bittitan.com>
2021-11-02 12:53:35 -0700

    =checkpoint

--- my-package/tasks/exec/exec.ps1
+++ my-package/tasks/exec/exec.ps1
@@ -1,10 +1,10 @@
 
-$context.SaveMessage("Information", "INFORMATION")
+$context.SaveMessage("Information", "Save Message INFORMATION")
 
 Start-Sleep -Seconds 5
 
-$context.SaveMessage("Warning", "WARNING")
+$context.SaveMessage("Warning", "Save Message WARNING")
 
 Start-Sleep -Seconds 5
 
-$context.SaveMessage("Error", "ERROR")
+$context.SaveMessage("Error", "Save Message ERROR")
