6519a8f269c349415618cb260c680f63dbd05675
Jaco Geldenhuys <jgeldenhuys@bittitan.com>
2021-11-02 10:08:05 -0700

    checkpoint

---
+++ my-package/tasks/exec2/exec2.ps1
@@ -0,0 +1,23 @@
+
+Write-Information "Started"
+
+Write-Warning "THIS IS A WARNING! AGAIN!"
+
+Start-Sleep -Seconds 5
+
+# Write-Error "THIS IS A ERROR!"
+
+# Start-Sleep -Seconds 5
+
+Write-Information "PEW PEW PEW ? WIAT WUT"
+
+# Write-Information "Exec Started"
+
+# try {
+#   Invoke-RestMethod -Uri "https://api.sendgrid.com/v3/mail/send"
+# }
+# catch {
+#   Write-Warning "Failed: $_"
+# }
+
+# Set-VoleerRunSummary -Summary "THIS IS THE RUN SUMMARY!"
